5c8ce12f75832f73fcc148416a81674aba0c58f4
Joonalai <joona.laine@cofactor.fi>
2025-04-28 13:58:30 +0300

    feat: add cprofiler for an alternative profiler method #9

---
+++ src/qgis_profiler/cprofiler.py
@@ -0,0 +1,224 @@
+#  Copyright (c) 2025 profiler-qgis-plugin contributors.
+#
+#
+#  This file is part of profiler-qgis-plugin.
+#
+#  profiler-qgis-plugin is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  profiler-qgis-plugin is distributed in the hope that it will be
+#  useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+#  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with profiler-qgis-plugin. If not, see <https://www.gnu.org/licenses/>.
+import cProfile
+import io
+import pstats
+from collections.abc import Generator, Sequence
+from contextlib import contextmanager
+from copy import deepcopy
+from dataclasses import dataclass, field
+from types import CodeType
+from typing import TYPE_CHECKING, Any, Union
+
+if TYPE_CHECKING:
+    from _lsprof import profiler_entry  # noqa: SC200
+
+
+@dataclass
+class ProfilerEntry:
+    """
+    Class representing a single profiling entry.
+
+    Inspired by _lsprof.profiler_entry and _lsprof.profiler_subentry
+    but made more flexible and easier to work with.
+    """
+
+    code: str  # code object or built-in function name
+    callcount: int = 1  # how many times this was called
+    inlinetime: float = 0.0  # inline time in this entry (not in subcalls)
+    reccallcount: int = 0  # how many times called recursively
+    totaltime: float = 0.0  # total time in this entry
+    calls: list["ProfilerEntry"] = field(default_factory=list)  # details of the calls
+
+    @staticmethod
+    def from_cprofiler(cprofiler: "cProfile.Profile") -> list["ProfilerEntry"]:
+        """Turns a cProfile.Profile stats into a list of ProfilerEntry objects."""
+        return [ProfilerEntry.from_stat(stat) for stat in cprofiler.getstats()]
+
+    @staticmethod
+    def from_stat(stat: "profiler_entry") -> "ProfilerEntry":
+        calls = (
+            []
+            if not hasattr(stat, "calls") or not stat.calls
+            else list(map(ProfilerEntry.from_stat, stat.calls))  # type: ignore
+        )
+        return ProfilerEntry(
+            stat.code.co_name if isinstance(stat.code, CodeType) else stat.code,
+            stat.callcount,
+            round(stat.inlinetime, 3),
+            round(stat.reccallcount, 3),
+            round(stat.totaltime, 3),
+            calls,
+        )
+
+    def __add__(self, other: "ProfilerEntry") -> "ProfilerEntry":
+        if self.code != other.code:
+            raise ValueError("Cannot add entries with different codes")  # noqa: TRY003
+        return ProfilerEntry(
+            self.code,
+            self.callcount + other.callcount,
+            self.inlinetime + other.inlinetime,
+            self.reccallcount,
+            self.totaltime + other.totaltime,
+            self.calls,
+        )
+
+    def _extend_calls(self, calls: list["ProfilerEntry"]) -> None:
+        call_dict = {call.code: call for call in self.calls}
+        for call in calls:
+            if call.code in call_dict:
+                call_dict[call.code] += call  # type: ignore
+            else:
+                call_dict[call.code] = call
+        self.calls = list(call_dict.values())
+
+    @staticmethod
+    def parse_from_qgis_profiler_text(text: str) -> list["ProfilerEntry"]:  # noqa: C901
+        """
+        Parses a given profiler text into a list of `ProfilerEntry` objects.
+        Processes hierarchical structure based on indentation and generates profiling
+        entries.
+        """
+
+        profile_entries: dict[str, ProfilerEntry] = {}
+
+        def profiler_lines_into_entries(
+            lines: list[str],
+            parents: set[str],
+            level: int = 1,
+        ) -> list["ProfilerEntry"]:
+            """Insert profiling data into the profile_entries dict."""
+            results = []
+            while lines:
+                line = lines[0]
+                line_level = line.count("-")
+                if line_level == level:
+                    # This line is at the current level
+                    lines.pop(0)
+                    parts = line.split(": ")
+                    name = parts[0].strip("- ").strip()
+                    total_time = round(float(parts[1].strip()), 3)
+                    entry = ProfilerEntry(name, totaltime=total_time)
+
+                    new_entry = name not in profile_entries
+                    if new_entry:
+                        profile_entries[name] = entry
+
+                    if name in parents:
+                        profile_entries[name].reccallcount += 1
+
+                    # Recursively parse children
+                    calls = profiler_lines_into_entries(
+                        lines, parents | {name}, level + 1
+                    )
+                    if calls:
+                        profile_entries[name]._extend_calls(calls)
+                    else:
+                        # No children, inline time should be total time
+                        entry.inlinetime = total_time
+
+                    if not new_entry:
+                        profile_entries[name] += entry
+
+                    if level > 1:
+                        if entry.calls:
+                            entry = deepcopy(entry)
+                            entry.calls = []
+                        results.append(entry)
+
+                elif line_level < level:
+                    # This line belongs to a parent level or is a group name
+                    break
+            return results
+
+        # The first line is the name of the group
+        profiler_lines_into_entries(
+            text.splitlines()[1:],
+            set(),
+        )
+        return list(profile_entries.values())
+
+
+class QCProfiler(cProfile.Profile):
+    """
+    cProfile.Profile subclass with QGIS-specific
+    functionality and extra utilities.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._qgis_stats: list[ProfilerEntry] = []
+        self._profiling: bool = False
+
+    @property
+    def is_profiling(self) -> bool:
+        return self._profiling
+
+    @contextmanager
+    def qgis_profiler_data(self, profiler_text: str) -> Generator[None, Any, None]:
+        self._qgis_stats = ProfilerEntry.parse_from_qgis_profiler_text(profiler_text)
+        try:
+            yield
+        finally:
+            self._qgis_stats = []
+
+    def enable(self, subcalls: bool = True, builtins: bool = True) -> None:  # noqa: FBT001 FBT002
+        super().enable(subcalls, builtins)
+        self._profiling = True
+
+    def disable(self) -> None:
+        super().disable()
+        self._profiling = False
+
+    def getstats(self) -> Sequence[Union[ProfilerEntry, "profiler_entry"]]:  # type: ignore[override]
+        if self._qgis_stats:
+            return self._qgis_stats
+        return super().getstats()
+
+    def get_stat_report(
+        self,
+        sort: Union[str, tuple[str, ...], int] = -1,
+        max_line_count: int = 1000,
+        trim_zeros: bool = False,  # noqa: FBT001 FBT002
+    ) -> str:
+        """
+        Get the profile report as a string.
+
+        :param sort: Sort method. Can be a string or a tuple of strings.
+        :param max_line_count: Maximum number of lines to return.
+        :param trim_zeros: Trim lines with zero times from the report.
+        :return: The profile report as a string.
+        """
+        if not isinstance(sort, tuple):
+            sort = (sort,)  # type: ignore[assignment]
+        with io.StringIO() as stream:
+            pstats.Stats(self, stream=stream).strip_dirs().sort_stats(  # type: ignore[misc]
+                *sort
+            ).print_stats()
+            report = stream.getvalue()
+        report_lines = []
+        if trim_zeros:
+            for line in report.splitlines():
+                if "0.000    0.000    0.000    0.000" in line:
+                    continue
+                report_lines.append(line)
+        else:
+            report_lines = report.splitlines()
+
+        # First 5 lines are part of the header
+        return "\n".join(report_lines[: max_line_count + 5])
---
+++ test/test_cprofiler.py
@@ -0,0 +1,206 @@
+#  Copyright (c) 2025 profiler-qgis-plugin contributors.
+#
+#
+#  This file is part of profiler-qgis-plugin.
+#
+#  profiler-qgis-plugin is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  profiler-qgis-plugin is distributed in the hope that it will be
+#  useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+#  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with profiler-qgis-plugin. If not, see <https://www.gnu.org/licenses/>.
+import logging
+import time
+from textwrap import dedent
+
+import pytest
+
+from qgis_profiler.cprofiler import ProfilerEntry, QCProfiler
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Class:
+    # @profile
+    def foo(self, a: int, b: int) -> int:
+        self.sleep(0.1)
+        return a + b + self.bar(a, b)
+
+    # @profile
+    def bar(self, a: int, b: int) -> int:
+        self.sleep(0.2)
+        if b == 0:
+            return a + self.foo(a, 1)
+        return a + b
+
+    # @profile
+    def sleep(self, amount: float) -> None:
+        time.sleep(amount)
+
+
+@pytest.fixture
+def cprofiler() -> QCProfiler:
+    return QCProfiler()
+
+
+@pytest.fixture
+def sample_text(default_group: str) -> str:
+    qgis_profiler_log = """
+    Plugins
+    - _import: 0.0
+    - foo: 0.3
+    -- sleep: 0.1
+    -- bar: 0.2
+    --- sleep: 0.2
+    - bar: 0.5
+    -- sleep: 0.2
+    -- foo: 0.3
+    --- sleep: 0.1
+    --- bar: 0.2
+    ---- sleep: 0.2
+    """
+    return dedent(qgis_profiler_log).strip()
+
+
+@pytest.fixture
+def parsed_profiler_entries(sample_text: str) -> list[ProfilerEntry]:
+    return ProfilerEntry.parse_from_qgis_profiler_text(sample_text)
+
+
+def test_parse_profile_entries_from_qgis_profiler_text(
+    parsed_profiler_entries: list[ProfilerEntry],
+):
+    assert parsed_profiler_entries == [
+        ProfilerEntry(
+            code="_import",
+            callcount=1,
+            inlinetime=0.0,
+            reccallcount=0,
+            totaltime=0.0,
+            calls=[],
+        ),
+        ProfilerEntry(
+            code="foo",
+            callcount=2,
+            inlinetime=0.0,
+            reccallcount=0,
+            totaltime=0.6,
+            calls=[
+                ProfilerEntry(
+                    code="sleep",
+                    callcount=2,
+                    inlinetime=0.2,
+                    reccallcount=0,
+                    totaltime=0.2,
+                    calls=[],
+                ),
+                ProfilerEntry(
+                    code="bar",
+                    callcount=2,
+                    inlinetime=0.0,
+                    reccallcount=0,
+                    totaltime=0.4,
+                    calls=[],
+                ),
+            ],
+        ),
+        ProfilerEntry(
+            code="sleep",
+            callcount=5,
+            inlinetime=0.8,
+            reccallcount=0,
+            totaltime=0.8,
+            calls=[],
+        ),
+        ProfilerEntry(
+            code="bar",
+            callcount=3,
+            inlinetime=0.0,
+            reccallcount=1,
+            totaltime=0.9,
+            calls=[
+                ProfilerEntry(
+                    code="sleep",
+                    callcount=3,
+                    inlinetime=0.6,
+                    reccallcount=0,
+                    totaltime=0.6,
+                    calls=[],
+                ),
+                ProfilerEntry(
+                    code="foo",
+                    callcount=1,
+                    inlinetime=0.0,
+                    reccallcount=0,
+                    totaltime=0.3,
+                    calls=[],
+                ),
+            ],
+        ),
+    ]
+
+
+def test_cprofiler_should_generate_stat_report_from_qgis_profiler_text(
+    cprofiler: QCProfiler, sample_text: str
+):
+    with cprofiler.qgis_profiler_data(sample_text):
+        report = cprofiler.get_stat_report("cumtime")
+    LOGGER.debug("\n%s", report)
+    assert (
+        report.strip()
+        == """\
+11 function calls (10 primitive calls) in 0.800 seconds
+
+   Ordered by: cumulative time
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+      3/2    0.000    0.000    0.900    0.450 bar
+        5    0.800    0.160    0.800    0.160 sleep
+        2    0.000    0.000    0.600    0.300 foo
+        1    0.000    0.000    0.000    0.000 _import"""
+    )
+
+
+def test_cprofiler_report_should_be_trimmed(cprofiler: QCProfiler, sample_text: str):
+    with cprofiler.qgis_profiler_data(sample_text):
+        report = cprofiler.get_stat_report("cumtime", max_line_count=2, trim_zeros=True)
+    LOGGER.debug("\n%s", report)
+    assert (
+        report.strip()
+        == """\
+11 function calls (10 primitive calls) in 0.800 seconds
+
+   Ordered by: cumulative time
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+      3/2    0.000    0.000    0.900    0.450 bar
+        5    0.800    0.160    0.800    0.160 sleep"""
+    )
+
+
+def test_cprofiler_should_profile_normally(cprofiler: QCProfiler, sample_text: str):
+    cprofiler.enable(builtins=False)
+    assert cprofiler.is_profiling
+    f = Class()
+    f.foo(1, 2)
+    f.bar(2, 0)
+    cprofiler.disable()
+    assert not cprofiler.is_profiling
+    entries = ProfilerEntry.from_cprofiler(cprofiler)
+
+    # It is rather challenging to compare the
+    # results directly since times might differ greatly
+    names = {entry.code for entry in entries}
+    assert names == {"bar", "foo", "is_profiling", "disable", "_import", "sleep"}
+    report = cprofiler.get_stat_report("cumtime")
+    LOGGER.info(report)
+    assert "bar" in report
+    assert "sleep" in report
+    assert "foo" in report
+    assert "_import" in report
